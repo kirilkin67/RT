579a2c990a5ab1af65cf320a0b86d435fae0c161
kirilkin67 <51540813+kirilkin67@users.noreply.github.com>
2020-03-17 11:06:12 +0300

    Update ft_ray_trace_sphere.c

--- src/ft_ray_trace_sphere.c
+++ src/ft_ray_trace_sphere.c
@@ -74,3 +74,64 @@ char			*ft_convert_base(char *nbr, char *base_from, char *base_to)
 	res = ft_strrev(res);
 	return (res);
 }
+
+
+int		ft_len_untill(const char *str, char c)
+{
+	int	len;
+
+	len = 0;
+	if (!str)
+		return (0);
+	while (str[len] != c && str[len] != '\0')
+		++len;
+	return (len);
+}
+
+double		ft_atof(const char *str)
+{
+	double	result;
+	int		i;
+
+	result = (double)ft_atoi(str);
+	i = ft_len_untill(str, '.');
+	if (i && str[i])
+	{
+		if (result >= 0 && str[0] != '-')
+			result += ((double)ft_atoi(str + i + 1))
+				/ ((ft_strlen(str + i + 1) * 10));
+		else
+			result -= ((double)ft_atoi(str + i + 1))
+				/ ((ft_strlen(str + i + 1) * 10));
+	}
+	return (result);
+}
+
+int		ft_ahextocolor(char *ahex)
+{
+	char	*str;
+	char	*tmp;
+	int		c;
+	int		res;
+
+	str = ft_strdup(ahex);
+	str[2] = '\0';
+	tmp = ft_convert_base(str, "0123456789ABCDEF", "0123456789");
+	c = ft_atoi(tmp);
+	res = c << 16;
+	free(str);
+	free(tmp);
+	str = ft_strdup(ahex + 2);
+	str[2] = '\0';
+	tmp = ft_convert_base(str, "0123456789ABCDEF", "0123456789");
+	c = ft_atoi(tmp);
+	res |= c << 8;
+	free(str);
+	free(tmp);
+	str = ft_strdup(ahex + 4);
+	tmp = ft_convert_base(str, "0123456789ABCDEF", "0123456789");
+	res |= ft_atoi(tmp);
+	free(str);
+	free(tmp);
+	return (res);
+}
